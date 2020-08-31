a981705be4cbebdeca082d9c97eaff78f8563917
Namkat cedrick jumtock <60654726+namkatcedrickjumtock@users.noreply.github.com>
2020-08-31 13:35:28 +0100

    inlcudes
    portfolio website project

---
+++ Folio/assets/js/jquery_min.js
@@ -0,0 +1,12 @@
+
+
+// About section 
+$(document).ready(function(){
+         $("#function_button").click((function(){
+                  $('#dynamic').toggle();
+                  // $('#dynamic').css("background-color", "blue");
+                  
+         }));
+         // $("#function_button").click((function(){
+                  // $('#dynamic').fadeOut()
+})
